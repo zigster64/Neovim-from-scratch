05a8f2a56a2a70397abeb786e924b021a44bacaf
Briain O'Driscoll <briainodriscoll@gmail.com>
2022-05-09 19:15:51 +0100

    amend global on_attach function

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -47,16 +47,6 @@ end
 --[=[
 local function lsp_highlight_document_original(client)
   -- Set autocommands conditional on server_capabilities
-    local status_ok, illuminate = pcall(require, "illuminate")
-    if not status_ok then
-      return
-    end
-    illuminate.on_attach(client)
-  -- end
-end
-]=]
-
-local function lsp_highlight_document(client)
   -- if client.server_capabilities.document_highlight then
     local status_ok, illuminate = pcall(require, "illuminate")
     if not status_ok then
@@ -93,9 +83,9 @@ end
 M.on_attach = function(client, bufnr)
 -- vim.notify(client.name .. " starting...")
 -- TODO: refactor this into a method that checks if string in list
-  if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
-  end
+--   if client.name == "tsserver" then
+--     client.resolved_capabilities.document_formatting = false
+--   end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
